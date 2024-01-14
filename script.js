0f3c5a9702484ab9f66f65ef492061a9d806db98
Sachin Kumar <123574186+Sachin6096@users.noreply.github.com>
2024-01-14 21:55:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let ans = "#"
+	
+	for (let x of shortHex)
+		{
+			if(x != '#')
+			ans = ans+x+x;
+			
+		}
+	return ans;
 };
 
 // Do not change the code below.
